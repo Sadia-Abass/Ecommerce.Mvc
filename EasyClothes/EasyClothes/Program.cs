ec584ad0155f6fb5ef1777ce10b0fd1f445b76e6
Sadia Abass <sadia.abass@outlook.com>
2024-08-14 11:18:22 +0100

    Add https configuration to lauchsettings.json file and add configuration for mvc in the program.cs file to the project.

--- EasyClothes/EasyClothes/Program.cs
+++ EasyClothes/EasyClothes/Program.cs
@@ -1,6 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to container
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+// Configure the HTTP request pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+
+
+//app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
