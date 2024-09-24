9c52288390071ab774ba72e6e346fbe5c3d946b7
Sadia Abass <sadia.abass@outlook.com>
2024-09-24 20:20:22 +0100

    Add custome properties to the identity user class.

--- EasyClothes/EasyClothes/Areas/Identity/Data/ApplicationUser.cs
+++ EasyClothes/EasyClothes/Areas/Identity/Data/ApplicationUser.cs
@@ -9,5 +9,15 @@ namespace EasyClothes.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    [PersonalData]
+    public string Name { get; set; } = string.Empty;
+    [PersonalData]
+    public string Surname { get; set; } = string.Empty;
+    [PersonalData]
+    public string AddressLineOne { get; set; } = string.Empty;
+    [PersonalData]
+    public string AddressLineTwo { get; set;} = string.Empty;
+    [PersonalData]
+    public string PostCode { get; set; } = string.Empty;
 }
 
