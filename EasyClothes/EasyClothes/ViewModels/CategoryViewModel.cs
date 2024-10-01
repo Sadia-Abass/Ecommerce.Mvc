cce69d262708fe2624c6d81e42fbb0566e3ab252
Sadia Abass <sadia.abass@outlook.com>
2024-10-01 21:23:05 +0100

    Add required property and data annotation to CategoryViewModel.

--- EasyClothes/EasyClothes/ViewModels/CategoryViewModel.cs
+++ EasyClothes/EasyClothes/ViewModels/CategoryViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace EasyClothes.ViewModels
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EasyClothes.ViewModels
 {
     public class CategoryViewModel
     {
+        [Required(ErrorMessage = "Category Title is required")]
+        [DisplayName("Category Title")]
+        public string Name { get; set; } = string.Empty;
     }
 }
